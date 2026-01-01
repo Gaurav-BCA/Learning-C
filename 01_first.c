1ef6cfeea345e4f5e45f84ea673901395d732296
Gaurav Bisht <gauravbisht059@gmail.com>
2026-01-01 23:45:09 +0530

    First Commit

---
+++ 01_first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello Guys");
+
+    printf(" I am Gaurav");
+    return 0;
+}
---
+++ _escape_sequence.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    char ch = 'a';
+    ch ='c';
+    ch = '\n'; //responses a new line
+    printf("hey i am good \\n i am gaurav ");
+    return 0;
+
+}
